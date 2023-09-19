353c45de72369606bfdcc9b8df34638b223da96b
shankar7042 <shankar.kumar.4171@gmail.com>
2023-09-19 18:50:32 +0530

    rename use-origin

--- src/components/modals/invite-modal.tsx
+++ src/components/modals/invite-modal.tsx
@@ -14,7 +14,7 @@ import { Label } from "@/components/ui/label";
 import { useModal } from "@/hooks/use-modal-store";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { useOrigin } from "@/hooks/use-hooks";
+import { useOrigin } from "@/hooks/use-origin";
 
 export const InviteModal = () => {
   const { onOpen, isOpen, type, onClose, data } = useModal();
